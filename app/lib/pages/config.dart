3461991e3657c1878c889a11b71ab3068ff3bc1b
MADMAN-Modding <mad@kmdtech.net>
2024-04-18 11:43:46 -0400

    Controls is getting closer wahoo

--- app/lib/pages/config.dart
+++ app/lib/pages/config.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+class ConfigPage extends StatefulWidget {
+  const ConfigPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _ConfigPage();
+}
+
+class _ConfigPage extends StatelessWidget {
+  @override
+
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [Text("Test")],
+    );
+  }
+ 
+}
