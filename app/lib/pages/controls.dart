be6848f3c46610cce3a68ca2f7454781da063501
MADMAN-Modding <mad@kmdtech.net>
2024-04-26 17:59:24 -0400

    Scores work and I updated the home page

--- app/lib/pages/controls.dart
+++ app/lib/pages/controls.dart
@@ -11,52 +11,156 @@ class ControlsPage extends StatefulWidget {
 class _ControlsPage extends State<ControlsPage> {
   String chosenPath = "";
 
-  var directory = TextEditingController();
+  var leftScore = TextEditingController();
+  var rightScore = TextEditingController();
   var jsonHandler = JSONHandler();
 
   @override
   Widget build(BuildContext context) {
+    leftScore.text = jsonHandler.readOverlay('scoreLeft');
+    rightScore.text = jsonHandler.readOverlay('scoreRight');
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Row(
+        const Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          // Left Team Wins
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Left Team Wins',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ]),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 4),
+            child: ElevatedButton(
+                onPressed: () {
+                  jsonHandler.writeOverlay('winsLeft', '0');
+                },
+                child: const Text("0")),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 4),
+            child: ElevatedButton(
+                onPressed: () {
+                  jsonHandler.writeOverlay('winsLeft', '1');
+                },
+                child: const Text('1')),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 4),
+            child: ElevatedButton(
+                onPressed: () {
+                  jsonHandler.writeOverlay('winsLeft', '2');
+                },
+                child: const Text('2')),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                jsonHandler.writeOverlay('winsLeft', '3');
+              },
+              child: const Text('3')),
+        ]),
+        // Right Team Wins
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Padding(
-              padding: EdgeInsets.all(16.0),
+            Padding(
+              padding: EdgeInsets.all(8.0),
               child: Text(
-                'Left Wins',
+                "Right Team Wins",
                 style: TextStyle(color: Colors.white),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 4),
-              child: ElevatedButton(
-                  onPressed: () {
-                    jsonHandler.writeOverlay('winsLeft', '0');
-                  },
-                  child: Text("0")),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 4),
-              child: ElevatedButton(
-                  onPressed: () {
-                    jsonHandler.writeOverlay('winsLeft', '1');
-                  },
-                  child: Text('1')),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 4),
-              child: ElevatedButton(
-                  onPressed: () {
-                    jsonHandler.writeOverlay('winsLeft', '2');
-                  },
-                  child: Text('2')),
-            ),
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
             ElevatedButton(
                 onPressed: () {
-                  jsonHandler.writeOverlay('winsLeft', '3');
+                  jsonHandler.writeOverlay('winsRight', '0');
                 },
-                child: Text('3'))
+                child: const Text('0')),
+            ElevatedButton(
+                onPressed: () {
+                  jsonHandler.writeOverlay('winsRight', '1');
+                },
+                child: const Text("1")),
+            ElevatedButton(
+                onPressed: () {
+                  jsonHandler.writeOverlay('winsRight', '2');
+                },
+                child: const Text('2')),
+            ElevatedButton(
+                onPressed: () {
+                  jsonHandler.writeOverlay('winsRight', '3');
+                },
+                child: const Text('3')),
+          ],
+        ),
+        const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Left and Right Scores",
+                style: TextStyle(color: Colors.white),
+              )
+            ],
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Left Score
+            SizedBox(
+                height: 50,
+                width: 35,
+                child: TextField(
+                  controller: leftScore,
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 2.0),
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 2.0),
+                    ),
+                    hintText: 'Left Score',
+                    hintStyle: TextStyle(color: Colors.white),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                  onChanged: (value) =>
+                      jsonHandler.writeOverlay('scoreLeft', value),
+                  textAlign: TextAlign.center,
+                )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                  height: 50,
+                  width: 35,
+                  child: TextField(
+                    controller: rightScore,
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width: 2.0),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width: 2.0),
+                      ),
+                      hintText: 'Right Score',
+                      hintStyle: TextStyle(color: Colors.white),
+                    ),
+                    style: const TextStyle(color: Colors.white),
+                    onChanged: (value) =>
+                        jsonHandler.writeOverlay('scoreRight', value),
+                    textAlign: TextAlign.center,
+                  )),
+            )
           ],
         )
       ],
