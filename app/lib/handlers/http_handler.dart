26aa7ae3b3060820eb6201fb57f92387b070ad9b
MADMAN-Modding <mad@kmdtech.net>
2024-05-24 23:56:39 -0400

    Starting work on a pure http server

---
+++ app/lib/handlers/http_handler.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_popup_card/flutter_popup_card.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_static/shelf_static.dart';
+import 'json_handler.dart';
+
+JSONHandler jsonHandler = JSONHandler();
+
+class HTTPHandler {
+  var handler = createStaticHandler("/home/mad/Desktop/Overlay/",
+      defaultDocument: 'index.html');
+
+  late var server;
+
+  Future<void> startServer(BuildContext context) async {
+    try {
+      var server = await shelf_io.serve(handler, 'localhost', 8080);
+      this.server = server;
+      popUpMaker("Server started", context);
+    } catch (e) {
+      popUpMaker("Server failed to start or is already running", context);
+    }
+  }
+
+  void stopServer(BuildContext context) {
+    try {
+      server.close();
+      popUpMaker("Server stopped", context);
+    } catch (e) {
+      popUpMaker("Failed to stop Server, did you start it?", context);
+    }
+  }
+
+  Future popUpMaker(String text, BuildContext context) {
+    return showPopupCard(
+      context: context,
+      builder: (context) {
+        return PopupCard(
+          elevation: 8,
+          color: const Color.fromARGB(255, 255, 255, 255),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(text),
+          ),
+        );
+      },
+      offset: const Offset(-16, 70),
+      alignment: Alignment.topRight,
+      useSafeArea: true,
+    );
+  }
+}
---
+++ app/lib/pages/http.dart
@@ -0,0 +1,59 @@
+import 'package:falcons_esports_overlays_controller/constants.dart';
+import 'package:falcons_esports_overlays_controller/handlers/php_server_handler.dart';
+import 'package:flutter/material.dart';
+import '../handlers/git_handler.dart';
+import '../handlers/json_handler.dart';
+
+class HTTPPage extends StatefulWidget {
+  const HTTPPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HTTPPage();
+}
+
+class _HTTPPage extends State<HTTPPage> {
+  String chosenPath = "";
+  PHPServerHandler http = PHPServerHandler();
+  GitHandler gitHandler = GitHandler();
+  JSONHandler jsonHandler = JSONHandler();
+
+  var directory = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                    onPressed: () async {
+                      if (await gitHandler
+                          .checkRepo(jsonHandler.readConfig("path"))) {
+                        http.startServer("");
+                      } else if (await gitHandler.checkRepo(
+                          "${jsonHandler.readConfig("path")}${Constants.slashType}FalconsEsportsOverlays")) {
+                        http.startServer("FalconsEsportsOverlays");
+                      }
+                    },
+                    child: const Text("Start Web Server")),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                    onPressed: () {
+                      http.stopServer();
+                    },
+                    child: const Text("Stop Web Server")),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
