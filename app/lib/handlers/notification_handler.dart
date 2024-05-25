20f94af860d5d7b4022bc2e56c71b974260a0498
MADMAN-Modding <mad@kmdtech.net>
2024-05-25 11:43:40 -0400

    Bug fixes

---
+++ app/lib/handlers/notification_handler.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_popup_card/flutter_popup_card.dart';
+
+class NotificationHandler {
+  static Future notification(BuildContext context, String text) {
+    return showPopupCard(
+      context: context,
+      builder: (context) {
+        return PopupCard(
+          elevation: 8,
+          color: const Color.fromARGB(255, 255, 255, 255),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(text),
+          ),
+        );
+      },
+      offset: const Offset(-16, 70),
+      alignment: Alignment.topRight,
+      useSafeArea: true,
+    );
+  }
+}
