23946413d5c110c2628eda451e8e6c407994f844
MADMAN-Modding <mad@kmdtech.net>
2024-05-25 01:44:10 -0400

    Converted ssbu to js

--- js/ssbu.js
+++ js/ssbu.js
@@ -1,35 +1,39 @@
-setInterval(function () {
-    fetch('../json/overlay.json')
+fetch('../json/overlay.json')
         .then((response) => response.json())
         .then((jsonData) => {
-            const winsLeft = +jsonData["winsLeft"];
-            const winsRight = +jsonData["winsRight"];
-
-            // Update the left side rounds won
-            for (let i = 1; i <= 2; i++) {
-                document.getElementById(`leftRoundsWon${i}`).style.backgroundColor = winsLeft >= i ? "#BE0F34" : "black";
+            for (const key in jsonData) {
+                outputJSON(key, jsonData[key]);
             }
+            
+            setInterval(function () {
+                const winsLeft = +jsonData["winsLeft"];
+                const winsRight = +jsonData["winsRight"];
+                
+                // Update the left side rounds won
+                for (let i = 1; i <= 2; i++) {
+                    document.getElementById(`leftRoundsWon${i}`).style.backgroundColor = winsLeft >= i ? "#BE0F34" : "black";
+                }
 
-            // Update the right side rounds won
-            for (let i = 1; i <= 2; i++) {
-                document.getElementById(`rightRoundsWon${i}`).style.backgroundColor = winsRight >= i ? jsonData["teamColorRight"] : "black";
-            }
+                // Update the right side rounds won
+                for (let i = 1; i <= 2; i++) {
+                    document.getElementById(`rightRoundsWon${i}`).style.backgroundColor = winsRight >= i ? jsonData["teamColorRight"] : "black";
+                }
 
-            // Updates the Team Names and week
-            document.getElementById("teamNameLeft").innerHTML = jsonData["teamNameLeft"];
-            document.getElementById("teamNameRight").innerHTML = jsonData["teamNameRight"];
-            document.getElementById("week").innerHTML = jsonData["week"];
-            document.getElementById("playerNamesLeft").innerHTML = jsonData["playerNamesLeft"];
-            document.getElementById("playerNamesRight").innerHTML = jsonData["playerNamesRight"];
-            document.getElementById("scoreLeft").innerHTML = jsonData["scoreLeft"];
-            document.getElementById("scoreRight").innerHTML = jsonData["scoreRight"];
+                // Updates the Team Names and week
+                document.getElementById("teamNameLeft").innerHTML = jsonData["teamNameLeft"];
+                document.getElementById("teamNameRight").innerHTML = jsonData["teamNameRight"];
+                document.getElementById("week").innerHTML = jsonData["week"];
+                document.getElementById("playerNamesLeft").innerHTML = jsonData["playerNamesLeft"];
+                document.getElementById("playerNamesRight").innerHTML = jsonData["playerNamesRight"];
+                document.getElementById("scoreLeft").innerHTML = jsonData["scoreLeft"];
+                document.getElementById("scoreRight").innerHTML = jsonData["scoreRight"];
 
-            // Team Color Changing for left and right
-            // Left
-            document.getElementById("namePlateLeft").style.backgroundColor = jsonData["teamColorLeft"];
-            document.getElementById("namePlateLeftSlope").style.backgroundColor = jsonData["teamColorLeft"];
-            // Right
-            document.getElementById("namePlateRight").style.backgroundColor = jsonData["teamColorRight"];
-            document.getElementById("namePlateRightSlope").style.backgroundColor = jsonData["teamColorRight"];
+                // Team Color Changing for left and right
+                // Left
+                document.getElementById("namePlateLeft").style.backgroundColor = jsonData["teamColorLeft"];
+                document.getElementById("namePlateLeftSlope").style.backgroundColor = jsonData["teamColorLeft"];
+                // Right
+                document.getElementById("namePlateRight").style.backgroundColor = jsonData["teamColorRight"];
+                document.getElementById("namePlateRightSlope").style.backgroundColor = jsonData["teamColorRight"];
         });
 }, 500);
