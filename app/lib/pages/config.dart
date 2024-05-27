bc97791f02f285bd65238bd236e55fed662f842a
MADMAN-Modding <mad@kmdtech.net>
2024-05-27 13:40:44 -0400

    Fixed some color bugs

--- app/lib/pages/config.dart
+++ app/lib/pages/config.dart
@@ -1,5 +1,4 @@
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:falcons_esports_overlays_controller/handlers/json_handler.dart';
 
