98fe1ba95e86e0a2da2bb44a2504c9692fa0cb7e
MADMAN-Modding <mad@kmdtech.net>
2024-06-14 21:00:57 -0400

    Stuff that may have been left out idk

---
+++ app/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/mad/Documents/Apps/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/mad/Documents/Coding/Projects/FalconsEsportsOverlays/app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "2.2.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 2 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 2 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/mad/Documents/Apps/flutter"
+  "PROJECT_DIR=/home/mad/Documents/Coding/Projects/FalconsEsportsOverlays/app"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC81NWVhZTY4NjRiMjk2ZGQ5ZjQzYjJjYzc1NzdlYzI1NmU1YzMyYThkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/mad/Documents/Coding/Projects/FalconsEsportsOverlays/app/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/mad/Documents/Coding/Projects/FalconsEsportsOverlays/app/lib/main.dart"
+)
