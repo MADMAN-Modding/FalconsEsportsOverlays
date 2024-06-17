f37da94b31bb148e0be86423f3752f6af5d055fe
MADMAN-Modding <mad@kmdtech.net>
2024-06-16 22:36:50 -0400

    Home side of overwatch is done

--- js/overwatch.js
+++ js/overwatch.js
@@ -8,12 +8,12 @@ setInterval(function () {
             const winsRight = +jsonData["winsRight"];
 
             // Update the left side rounds won
-            for (let i = 1; i <= 2; i++) {
+            for (let i = 1; i <= 3; i++) {
                 document.getElementById(`leftRoundsWon${i}`).style.backgroundColor = winsLeft >= i ? "white" : "black";
             }
 
             // Update the right side rounds won
-            for (let i = 1; i <= 2; i++) {
+            for (let i = 1; i <= 3; i++) {
                 document.getElementById(`rightRoundsWon${i}`).style.backgroundColor = winsRight >= i ? "white" : "black";
             }
             
@@ -21,7 +21,7 @@ setInterval(function () {
             let teamColorLeft = jsonData["teamColorLeft"];
             changeBackgroundColor("teamNameLeftBackground", teamColorLeft);
             changeBackgroundColor("leftRoundsBack", teamColorLeft);
-
+            changeBackgroundColor("week", teamColorLeft);
 
             // Set the away team respective colors
             let teamColorRight = jsonData["teamColorRight"];
