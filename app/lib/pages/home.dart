92ce2bdc9f05c58b26876f7268e0139a129d4ecf
MADMAN-Modding <mad@kmdtech.net>
2024-07-05 01:16:02 -0400

    updated homepage

--- app/lib/pages/home.dart
+++ app/lib/pages/home.dart
@@ -60,7 +60,7 @@ class HomePage extends StatelessWidget {
               Padding(
                 padding: EdgeInsets.only(left: 8, top: 2),
                 child: SelectableText(
-                  "0. Make sure git is added to the PATH, you can test this with git -v in a command line \n1. Go to config to set your settings\n2. Go to Download & Update to get the overlay\n3. Go to Web Server to start the server\n4. Finally now you can control the overlay from the Overlay Data Page\n5. Make sure to add the overlay as a browser in OBS\nThe URL is http://localhost:8080 and the dimensions are 1920x1080\n6. You can change the falcon image by going to the controls page and pressing the image button",
+                  "0. Make sure git is added to the PATH, you can test this with git -v in a command line \n1. Go to config to set your settings\n2. Go to Download & Update to get the overlay\n3. Go to Web Server to start the server\n4. Finally now you can control the overlay from the Overlay Data Page\n5. Make sure to add the overlay as a browser in OBS\n\t\t\t\t\tThe URL is http://localhost:8080 and the dimensions are 1920x1080\n6. You can change the falcon image by going to the controls page and pressing the image button\n7. You can change the enabled sports in the config page",
                   style: TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
