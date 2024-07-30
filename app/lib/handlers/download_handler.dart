28796b2cb12aea0b1bd68af6c8b34be10b2b324f
MADMAN-Modding <mad@kmdtech.net>
2024-07-30 16:04:20 -0400

    Downloading the overlay and unzipping it works

--- app/lib/handlers/download_handler.dart
+++ app/lib/handlers/download_handler.dart
@@ -1,14 +1,23 @@
 import 'dart:io';
+import 'package:archive/archive_io.dart';
 import 'package:http/http.dart' as http;
+import 'package:permission_handler/permission_handler.dart';
 
 class DownloadHandler {
-  static download(String path) {
-    http
-        .get(Uri.https('codeload.github.com',
-            '/MADMAN-Modding/FalconsEsportsOverlays/zip/refs/heads/main'))
-        .then((response) {
-      File(path).writeAsBytesSync(response.bodyBytes);
-      print(path);
-    });
+  static download(String path) async {
+    if (Platform.isLinux || await Permission.storage.request().isGranted) {
+      await http
+          .get(Uri.https('codeload.github.com',
+              '/MADMAN-Modding/FalconsEsportsOverlays/zip/refs/heads/main'))
+          .then((response) {
+        File("$path/overlay.zip").writeAsBytesSync(response.bodyBytes);
+      });
+
+      extractor(path);
+    }
+  }
+
+  static extractor(String path) {
+    extractFileToDisk("$path/overlay.zip", path);
   }
 }
--- app/lib/pages/android_download.dart
+++ app/lib/pages/android_download.dart
@@ -1,6 +1,8 @@
-import 'package:falcons_esports_overlays_controller/constants.dart';
+import 'package:falcons_esports_overlays_controller/constants.dart'
+    as constants;
 import 'package:falcons_esports_overlays_controller/handlers/download_handler.dart';
 import 'package:falcons_esports_overlays_controller/handlers/json_handler.dart';
+import 'package:falcons_esports_overlays_controller/handlers/notification_handler.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -12,13 +14,10 @@ class AndroidDownloadPage extends StatefulWidget {
 }
 
 class _AndroidDownloadPage extends State<AndroidDownloadPage> {
-  late String chosenPath = Constants.executableDir;
+  String chosenPath = constants.Constants.executableDir;
 
   @override
   Widget build(BuildContext context) {
-    // Sets the chosen values
-    chosenPath = Constants.codePath;
-
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -32,7 +31,10 @@ class _AndroidDownloadPage extends State<AndroidDownloadPage> {
                   padding: const EdgeInsets.all(2.0),
                   child: ElevatedButton(
                     onPressed: () async {
-                      DownloadHandler.download(chosenPath);
+                      await DownloadHandler.download(chosenPath);
+
+                      NotificationHandler.notification(
+                          context, "Overlays Downloaded");
                     },
                     child: const Text('Download Overlays'),
                   ),
