7a9bee3f2779467534e574f068df139d5acb3dbd
MADMAN-Modding <mad@kmdtech.net>
2024-07-30 22:52:49 -0400

    fixed iframe access to work when not using localhost

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
 let overlayiFrame;
 let i = 0;
 setInterval(function () {
-        fetch('../json/overlay.json?' + i)
-            .then((response) => response.json())
-            .then((jsonData) => {
-                overlayiFrame = document.getElementById('overlayiFrame');
+  fetch("../json/overlay.json?" + i)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      overlayiFrame = document.getElementById("overlayiFrame");
 
-                // Returns if the overlay is already set
-                if (overlayiFrame.src.includes(jsonData['overlay'])) {
-                    return;
-                }
+      // Returns if the overlay is already set
+      if (overlayiFrame.src.includes(jsonData["overlay"])) {
+        return;
+      }
 
-                // Loads the correct overlay
-                overlayiFrame.src = "http://localhost:8080/overlays/" + jsonData["overlay"] + ".html";
+      // Loads the correct overlay
+      overlayiFrame.src =
+        window.location.href + "/overlays/" + jsonData["overlay"] + ".html";
 
-                document.title = jsonData["overlay"].charAt(0).toUpperCase() + jsonData["overlay"].slice(1)
-            })
-    i = Math.floor(Math.random() * 1000000);
-}, 200)
+      document.title =
+        jsonData["overlay"].charAt(0).toUpperCase() +
+        jsonData["overlay"].slice(1);
+    });
+  i = Math.floor(Math.random() * 1000000);
+}, 200);
