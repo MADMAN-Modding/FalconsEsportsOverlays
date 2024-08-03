a1e9e346a4f03a3507ca2358dd97a6effdc0e6aa
MADMAN-Modding <mad@kmdtech.net>
2024-08-02 21:12:32 -0400

    Made the app scrollable on android

--- app/lib/pages/config.dart
+++ app/lib/pages/config.dart
@@ -49,180 +49,17 @@ class _ControlsPage extends State<ConfigPage> {
     } catch (e) {
       logo = FileImage(File("path"));
     }
+    if (!Platform.isAndroid) {
+      return appPage(directory, logo, appTheme);
+    }
+
     return SizedBox(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          // Code Directory
-          if (!Platform.isAndroid) ...[
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 13),
-                  child: Text(
-                    "This is the directory that contains all the code",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                // Makes a box for the input
-                SizedBox(
-                  height: 50,
-                  width: 50,
-                  // A button with the folder icon that opens up a file-picker in order to chose the appropriate directory
-                  child: TextButton(
-                    child: const Icon(
-                      Icons.folder,
-                      color: Colors.white,
-                    ),
-                    onPressed: () async {
-                      String newCodePath =
-                          (await FolderPicker.folderPicker(context));
-
-                      // If the returned path is blank then it won't take the value
-                      codePath = newCodePath == "" ? codePath : newCodePath;
-
-                      // Sets the value of the directory text editing controller
-                      directory.text = codePath
-                          .toString(); // Sets the text equal to the path
-                      JSONHandler().writeConfig('path', codePath.toString());
-                    },
-                  ),
-                ),
-
-                // Sets the size of the textfield and also does some stuff with the controller
-                TextEditor.textEditor(
-                    width: 550,
-                    height: 40,
-                    controller: directory,
-                    label: "",
-                    boxHeight: 0,
-                    onChange: true),
-              ],
-            ),
-          ],
-          // Makes the image button and the text
-          Row(
-            children: [
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        height: 50,
-                        width: 50,
-                        child: TextButton(
-                          child: const Icon(
-                            Icons.image,
-                            color: Colors.white,
-                          ),
-                          onPressed: () async {
-                            String newImagePath =
-                                (await FilePick.filePicker(context));
-
-                            // If the returned path is blank then it won't take the value
-                            if (newImagePath != "") {
-                              imagePath = newImagePath;
-                              try {
-                                // Writes the old logo with the supplied path, this has both items as FileImages
-                                logo.file.writeAsBytesSync(FileImage(
-                                        File(imagePath.replaceAll(r"\", r"\\")))
-                                    .file
-                                    .readAsBytesSync());
-                              } catch (e) {}
-                            }
-                            // Sets the text equal to the path
-                            directory.text = codePath.toString();
-
-                            // Clears the cache
-                            logo.evict();
-                          },
-                        ),
-                      ),
-                      DefaultText.text(
-                          "Set the image you would like to use for the overlay icon. "),
-                    ],
-                  ),
-                  // Displays the logo
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Image.file(
-                          logo.file,
-                          width: 400 * (Platform.isAndroid ? 0.4 : 1),
-                        )
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        DefaultText.text(
-                            "App Theme (Change page for color changes to apply)")
-                      ],
-                    ),
-                  ),
-                  // Makes a color picker that is used for the apps theme
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        ColorSelector.colorPicker(
-                            color: HexColor(constants
-                                .Constants.jsonHandler.configJSON["appTheme"]),
-                            colorController: appTheme),
-                        TextEditor.textEditor(
-                            width: 200 * constants.Constants.multiplier,
-                            height: 40,
-                            controller: appTheme,
-                            label: "",
-                            boxHeight: 40,
-                            onChange: true,
-                            key: "appTheme")
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Column(
-                        children: [
-                          DefaultText.text("Choose your school sports!"),
-                          checkBoxMaker(
-                              "Super Smash Bros. Ultimate", "ssbuChecked"),
-                          checkBoxMaker("Mario Kart", "kartChecked"),
-                          checkBoxMaker("Overwatch", "owChecked"),
-                          checkBoxMaker("Rocket League", "rlChecked"),
-                          checkBoxMaker("Splatoon", "splatChecked"),
-                          checkBoxMaker("Valorant", "valChecked"),
-                          checkBoxMaker("Hearth Stone", "hearthChecked"),
-                          checkBoxMaker("League of Legends", "lolChecked"),
-                          checkBoxMaker("Chess", "chessChecked"),
-                          checkBoxMaker("Madden", "maddenChecked"),
-                          checkBoxMaker("NBA 2K", "nba2KChecked")
-                        ],
-                      )
-                    ],
-                  )
-                ],
-              )
-            ],
-          ),
-        ],
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: appPage(directory, logo, appTheme)),
       ),
     );
   }
@@ -245,6 +82,184 @@ class _ControlsPage extends State<ConfigPage> {
             });
           },
         )
+      ],
+    );
+  }
+
+  Widget appPage(TextEditingController directory, FileImage logo,
+      TextEditingController appTheme) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        // Code Directory
+        if (!Platform.isAndroid) ...[
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 13),
+                child: Text(
+                  "This is the directory that contains all the code",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              // Makes a box for the input
+              SizedBox(
+                height: 50,
+                width: 50,
+                // A button with the folder icon that opens up a file-picker in order to chose the appropriate directory
+                child: TextButton(
+                  child: const Icon(
+                    Icons.folder,
+                    color: Colors.white,
+                  ),
+                  onPressed: () async {
+                    String newCodePath =
+                        (await FolderPicker.folderPicker(context));
+
+                    // If the returned path is blank then it won't take the value
+                    codePath = newCodePath == "" ? codePath : newCodePath;
+
+                    // Sets the value of the directory text editing controller
+                    directory.text =
+                        codePath.toString(); // Sets the text equal to the path
+                    JSONHandler().writeConfig('path', codePath.toString());
+                  },
+                ),
+              ),
+
+              // Sets the size of the textfield and also does some stuff with the controller
+              TextEditor.textEditor(
+                  width: 550,
+                  height: 40,
+                  controller: directory,
+                  label: "",
+                  boxHeight: 0,
+                  onChange: true),
+            ],
+          ),
+        ],
+        // Makes the image button and the text
+        Row(
+          children: [
+            Column(
+              children: [
+                Row(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: TextButton(
+                        child: const Icon(
+                          Icons.image,
+                          color: Colors.white,
+                        ),
+                        onPressed: () async {
+                          String newImagePath =
+                              (await FilePick.filePicker(context));
+
+                          // If the returned path is blank then it won't take the value
+                          if (newImagePath != "") {
+                            imagePath = newImagePath;
+                            try {
+                              // Writes the old logo with the supplied path, this has both items as FileImages
+                              logo.file.writeAsBytesSync(FileImage(
+                                      File(imagePath.replaceAll(r"\", r"\\")))
+                                  .file
+                                  .readAsBytesSync());
+                            } catch (e) {}
+                          }
+                          // Sets the text equal to the path
+                          directory.text = codePath.toString();
+
+                          // Clears the cache
+                          logo.evict();
+                        },
+                      ),
+                    ),
+                    DefaultText.text(
+                        "Set the image you would like to use for the overlay icon. "),
+                  ],
+                ),
+                // Displays the logo
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Image.file(
+                        logo.file,
+                        width: 400 * (Platform.isAndroid ? 0.4 : 1),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      DefaultText.text(
+                          "App Theme (Change page for color changes to apply)")
+                    ],
+                  ),
+                ),
+                // Makes a color picker that is used for the apps theme
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      ColorSelector.colorPicker(
+                          color: HexColor(constants
+                              .Constants.jsonHandler.configJSON["appTheme"]),
+                          colorController: appTheme),
+                      TextEditor.textEditor(
+                          width: 200 * constants.Constants.multiplier,
+                          height: 40,
+                          controller: appTheme,
+                          label: "",
+                          boxHeight: 40,
+                          onChange: true,
+                          key: "appTheme")
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        DefaultText.text("Choose your school sports!"),
+                        checkBoxMaker(
+                            "Super Smash Bros. Ultimate", "ssbuChecked"),
+                        checkBoxMaker("Mario Kart", "kartChecked"),
+                        checkBoxMaker("Overwatch", "owChecked"),
+                        checkBoxMaker("Rocket League", "rlChecked"),
+                        checkBoxMaker("Splatoon", "splatChecked"),
+                        checkBoxMaker("Valorant", "valChecked"),
+                        checkBoxMaker("Hearth Stone", "hearthChecked"),
+                        checkBoxMaker("League of Legends", "lolChecked"),
+                        checkBoxMaker("Chess", "chessChecked"),
+                        checkBoxMaker("Madden", "maddenChecked"),
+                        checkBoxMaker("NBA 2K", "nba2KChecked")
+                      ],
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ],
     );
   }
--- app/lib/pages/home.dart
+++ app/lib/pages/home.dart
@@ -13,64 +13,75 @@ class HomePage extends StatelessWidget {
 
     String androidInstructions =
         "1. Go to Overlay Files to get the overlays\n2. Go to Config to set your settings\n3. Go to Web Server to start the server\n4. Finally now you can control the overlay from Overlay Data\n5. Make sure to add the overlay as a browser in OBS and the dimensions are 1920x1080\n\t\t\t\t\tThe URL is http://<your_ip>:8080 Your IP can be found in your network settings\n6. You can change the falcon image by going to the controls page and pressing the image button\n7. You can change the enabled sports in the config page";
-    return Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 8),
-                child: Text(
-                  'Welcome to the Falcons Esports Overlays Controller!',
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Text(
+                        'Welcome to the Falcons Esports Overlays Controller!',
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Text(
+                        'Created by: MADMAN-Modding',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+                const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 8, top: 2),
+                      child: Text(
+                        "Instructions",
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8, top: 2),
+                      child: SelectableText(
+                        (Platform.isAndroid
+                            ? androidInstructions
+                            : pcInstructions),
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
-          const Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 8),
-                child: Text(
-                  'Created by: MADMAN-Modding',
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontStyle: FontStyle.italic,
-                      color: Colors.white),
-                ),
-              ),
-            ],
-          ),
-          const Row(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(left: 8, top: 2),
-                child: Text(
-                  "Instructions",
-                  style: TextStyle(color: Colors.white, fontSize: 40),
-                ),
-              )
-            ],
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 8, top: 2),
-                child: SelectableText(
-                  (Platform.isAndroid ? androidInstructions : pcInstructions),
-                  style: const TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17),
-                ),
-              )
-            ],
-          )
-        ],
+        ),
       ),
     );
   }
