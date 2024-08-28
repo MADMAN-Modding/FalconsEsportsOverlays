64458a69ea13fa3b4910d59079b6794b8c3d16d3
MADMAN-Modding <mad@kmdtech.net>
2024-08-27 20:52:38 -0400

    removed unneeded import

--- app/lib/handlers/http_handler.dart
+++ app/lib/handlers/http_handler.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:falcons_esports_overlays_controller/handlers/notification_handler.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
