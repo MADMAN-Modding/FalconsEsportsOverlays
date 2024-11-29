dae3afda585d6e74df7a3eb4bb02dd9e0598e013
MADMAN-Modding <mad@kmdtech.net>
2024-11-29 15:22:44 -0500

    control page buttons can update the overlay json, needs some css though

--- rust_app/src/js/switchPage.js
+++ rust_app/src/js/switchPage.js
@@ -1,10 +1,15 @@
-function switchPage(page) {
-    fetch(`pages/${page}.html`)
+async function switchPage(page) {
+    await fetch(`pages/${page}.html`)
     .then(response => response.text())
     .then(html => {
         document.getElementById("page").innerHTML = html;
     })
     .catch(error => console.error("Error loading page:", error));
+
+    if (page == "controls") {
+        console.log("Hi from Controls")
+        buildTeamColumn("Left", "leftTeamColumn")
+    }
 }
 
 switchPage("main");
