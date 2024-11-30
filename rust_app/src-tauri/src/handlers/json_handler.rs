0e02d11881fbaee928acd93bc7668e700356c912
MADMAN-Modding <mad@kmdtech.net>
2024-11-30 10:12:18 -0500

    overlay json file will now be made if it doesn't exist

--- rust_app/src-tauri/src/handlers/json_handler.rs
+++ rust_app/src-tauri/src/handlers/json_handler.rs
@@ -1,6 +1,6 @@
-use std::fs;
+use std::{fs, path::Path};
 
-use serde_json::Value;
+use serde_json::{json, Value};
 
 use crate::constants;
 
@@ -12,16 +12,74 @@ pub fn read_overlay_json(key : &str) -> String {
 }
 
 fn open_json(path: String) -> Value {
-    let json_data: Value = {
-        let file_content: String = fs::read_to_string(path).expect("Error reading file");
-        serde_json::from_str::<Value>(&file_content).expect("Error serializing to JSON")
-    };
+    let json_data: Value;
+
+    // Checks to make sure that the JSON file is there, if it isn't it makes it
+    if Path::new(&path).exists() {
+        println!("Path exists");
+        json_data = {
+            let file_content: String = fs::read_to_string(&path).expect("File not found");
+            serde_json::from_str::<Value>(&file_content).expect("Error serializing to JSON")
+        };
+    } else {
+        println!("Making new JSON");
+        json_data = init_json(path);
+    }
 
     // Prints the entire JSON, mainly for debugging 
     // println!("{:?}", serde_json::to_string_pretty(&json_data).expect("Error parsing to JSON"));
 
     // Returns the json data
     json_data
+}
+
+fn init_json(path: String) -> Value {
+    // Creating the directories
+    let _ = std::fs::create_dir_all(Path::new(&path).parent().unwrap());
+
+    let json_data: Value;
+
+    if path.contains("overlay.json") {
+        json_data = json!({
+            "teamNameLeft": "DC Falcons Red",
+            "teamNameRight": "That other team",
+            "winsLeft": "0",
+            "winsRight": "0",
+            "teamColorLeft": "#BE0F32",
+            "teamColorRight": "#0120AC",
+            "overlay": "kart",
+            "week": "0",
+            "scoreLeft": "0",
+            "scoreRight": "0",
+            "playerNamesLeft": "MADMAN-Modding",
+            "playerNamesRight": "Check out my Github"
+        });
+    } else {
+        json_data = json!({
+            "appTheme": "#bf0f35",
+            "ssbuChecked": true,
+            "kartChecked": true,
+            "owChecked": true,
+            "rlChecked": true,
+            "splatChecked": true,
+            "valChecked": true,
+            "hearthChecked": true,
+            "lolChecked": true,
+            "chessChecked": true,
+            "maddenChecked": true,
+            "nba2KChecked": true
+        })
+    }
+
+    println!("Creating the JSON file: {}", &path);
+    // Creating the JSON file
+    fs::write(&path, serde_json::to_string_pretty(&json_data).expect("Error 
+    serializing to JSON")).expect("Error writing file");
+
+     
+    println!("Trying to open the JSON");
+    // Trying to open the JSON again
+    open_json(path)
 }
 
 /**
