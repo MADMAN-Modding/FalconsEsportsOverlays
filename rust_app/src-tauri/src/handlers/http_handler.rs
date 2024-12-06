df3f68e81edec4e2b4dd1fbcf3a9256eec3e17df
MADMAN-Modding <mad@kmdtech.net>
2024-12-06 08:24:16 -0500

    almost finished the http server, need to add spawning for the process and logic to handle '?' in resource URI's

--- rust_app/src-tauri/src/handlers/http_handler.rs
+++ rust_app/src-tauri/src/handlers/http_handler.rs
@@ -2,12 +2,14 @@ use std::{
     fs, io::{prelude::*, BufReader}, net::TcpListener, path::Path
 };
 
+use crate::constants::get_code_dir;
+
 #[tauri::command]
 pub async fn run_server () -> Result<String, String> {
     let listener = TcpListener::bind("127.0.0.1:8080");
 
     if listener.is_ok() {
-        handle_connection(listener.unwrap());
+        handle_connection(listener.unwrap()).await;
         Ok("Server Started".to_string())
     } else {
         Err(format!("Error Starting Server: {}", listener.unwrap_err()))
@@ -28,19 +30,24 @@ async fn handle_connection(listener: TcpListener) {
         file = file.replace("GET ", "").replace(" HTTP/1.1", "");
 
         file = if file == "/" {
-            "index.html".to_owned()
+            "/index.html".to_owned()
         } else {
-            file.replace("/", "")
+            file
         };
 
-        println!("{}", file);
+        let file_path = get_code_dir() + &file;
+
+        println!("{}", &file_path);
 
 
-        let (status_line, filename) = if Path::new(&file).exists() {
-            ("HTTP/1.1 200 OK", file)
+        // TODO: Add logic to handle a '?' on requested resouces
+        let (status_line, filename) = if Path::new(&file_path).exists() {
+            ("HTTP/1.1 200 OK", file_path)
         } else {
             ("HTTP/1.1 404 NOT FOUND", "404.html".to_string())
         };
+
+        println!("{}", &filename);
 
         let contents = fs::read_to_string(filename).unwrap();
         let length = contents.len();
