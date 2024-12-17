d2c3ab87a8ed3343b57d57dad1bf503688a9bedb
MADMAN-Modding <mad@kmdtech.net>
2024-12-17 18:10:40 -0500

    Removed the dart app

--- app/android/app/src/main/kotlin/com/example/falcons_esports_overlays_controller/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.madman_modding.falcons_esports_overlays_controller
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- app/android/app/src/main/kotlin/com/madman_modding/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.madman_modding.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
