c031a2e7e1b2f8667535a55eaf451470b9a0eb1d
MADMAN-Modding <mad@kmdtech.net>
2024-12-26 15:21:51 -0500

    added image for the server being on or off

--- rust_app/src/js/http_handler.js
+++ rust_app/src/js/http_handler.js
@@ -1,11 +1,19 @@
 async function start_server() {
     invoke('run_server')
-        .then((message) => push_notification(message))
+        .then((message) => {push_notification(message); serverOn();})
         .catch((error) => push_notification(error));
 }
 
-function stop_server() {
+async function stop_server() {
     invoke('stop_server')
-        .then((message) => push_notification(message))
+        .then((message) => {push_notification(message); serverOff();})
         .catch((error) => push_notification(error));
+}
+
+function serverOn() {
+    document.getElementById("serverImage").src = "../images/ServerOn.png";
+}
+
+function serverOff() {
+    document.getElementById("serverImage").src = "../images/ServerOff.png";
 }
