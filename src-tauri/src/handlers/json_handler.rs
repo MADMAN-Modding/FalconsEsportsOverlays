529d97ec25f41dfc1282e638c2d4b12eeea717e5
MADMAN-Modding <mad@kmdtech.net>
2025-04-21 22:18:44 -0400

    wrote a function to write to a nested json key using recursion and a function to count the amount of keys with a given name

--- src-tauri/src/handlers/json_handler.rs
+++ src-tauri/src/handlers/json_handler.rs
@@ -1,5 +1,10 @@
 //! This module is used for read and writing the json data used for the overlays and the app
-use std::{collections::HashMap, fs::{self, File}, io::{BufReader, Read}, path::Path};
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io::{BufReader, Read},
+    path::Path,
+};
 
 use serde_json::{json, Value};
 
@@ -8,47 +13,47 @@ use crate::constants::{self, get_launch_json_path, get_local_versions_path};
 use super::download_handler::download_files;
 
 /// Reads the overlay json and returns the value of the requested key
-/// 
+///
 /// # Arguments
 /// * `key: &str` - The key to be read from the json file
-/// 
+///
 /// # Returns
-/// * `String` - The data at the desired key 
+/// * `String` - The data at the desired key
 #[tauri::command]
 pub fn read_overlay_json(key: &str) -> String {
     read_json(key, constants::get_overlay_json_path())
 }
 
 /// Reads the config json and returns the value of the requested key
-/// 
+///
 /// # Arguments
 /// * `key: &str` - The key to be read from the json file
-/// 
+///
 /// # Returns
-/// * `String` - The data at the desired key 
+/// * `String` - The data at the desired key
 #[tauri::command]
 pub fn read_config_json(key: &str) -> String {
     read_json(key, constants::get_config_json_path())
 }
 
 /// Reads the config json and returns the value of the requested key
-/// 
+///
 /// # Arguments
 /// * `key: &str` - The key to be read from the json file
-/// 
+///
 /// # Returns
-/// * `String` - The data at the desired key 
+/// * `String` - The data at the desired key
 #[tauri::command]
 pub fn read_custom_json(key: &str) -> String {
     read_json(key, constants::get_custom_config_path())
 }
 
 /// Reads the json at the supplied path and returns the value of the requested key
-/// 
+///
 /// # Arguments
 /// * `key: &str` - The key to be read from the json file
 /// * `path: String` - The path to the json file
-/// 
+///
 /// #Returns
 /// * 'String' - The data at the desired key
 #[tauri::command]
@@ -58,35 +63,41 @@ pub fn read_json(key: &str, path: String) -> String {
     json_data[key].to_string().replace("\"", "")
 }
 
+pub fn read_json_as_value(path: String) -> Value {
+    open_json(path).clone()
+}
+
 /// Opens the json file with the supplied path
-/// 
+///
 /// # Arguments
 /// * `path: String` - The path to the JSON file to read
-/// 
+///
 /// # Returns
 /// * `Value` - Contains the JSON data
-/// 
+///
 /// # Examples
 /// ```ignore
 /// open_json("random_path/overlay.json");
 /// ```
 fn open_json(path: String) -> Value {
     let json_data: Value;
- 
+
     // Checks to make sure that the JSON file is there, if it isn't it makes it
     if Path::new(&path).exists() {
         let mut reader: BufReader<File> = BufReader::new(File::open(&path).unwrap());
 
         let mut buffer: Vec<u8> = Vec::new();
 
-        reader.read_to_end(&mut buffer).map_err(|e| e.to_string()).unwrap();
-
+        reader
+            .read_to_end(&mut buffer)
+            .map_err(|e| e.to_string())
+            .unwrap();
 
         // If the file is a "Resource Not Found" file, return a blank vector
         if buffer.len() == 0 {
-            return Value::default()
+            return Value::default();
         }
-        
+
         json_data = {
             let file_content: String = fs::read_to_string(&path).expect("File not found");
             serde_json::from_str::<Value>(&file_content).expect("Error serializing to JSON")
@@ -100,12 +111,12 @@ fn open_json(path: String) -> Value {
 }
 
 /// This function is called if the JSON being read doesn't exist
-/// 
+///
 /// It after making the file it will try to read the file and then return that value
-/// 
+///
 /// # Arguments
 /// * `path: String` - The path to the JSON file to read
-/// 
+///
 /// # Returns
 /// * `Value` - Contains the JSON data
 pub fn init_json(path: String) -> Value {
@@ -153,12 +164,12 @@ pub fn init_json(path: String) -> Value {
 }
 
 /// Writes to the JSON file at the supplied path
-/// 
+///
 /// # Arguments
 /// * `path: String` - Path to the JSON file
 /// * `json_key: String` - Key to write to
 /// * `value: String` ` Value to write to the key`
-/// 
+///
 /// # Examples
 /// ```ignore
 /// write_json("random_path/overlay.json", "overlay", "kart");
@@ -201,8 +212,146 @@ pub fn write_json(path: String, json_key: String, mut value: String) {
     .expect("Error writing file");
 }
 
+
+/// Recursively reads a JSON value and writes a new value to the specified key path.
+/// No IO means it won't write to file system
+/// 
+/// # Arguments
+/// * `json` - The JSON value to be modified.
+/// * `keys` - A dot-separated string path specifying the keys/indexes to traverse. Array indices should be wrapped in square brackets, `arrayKey[0].nestedKey`.
+/// * `value` - The new value to write at the final key.
+/// 
+/// # Returns
+/// * `Value` - The modified JSON with the new value inserted.
+/// 
+/// # Example
+/// ```ignore
+/// use serde_json::{json, Value};
+///
+/// let json = json!({"key": [{"nestedKey": "oldValue"}]});
+///
+/// let value = Value::String("newValue".to_string());
+///
+/// let new_json = write_nested_json_no_io(json, "key[0].nestedKey".to_string(), value);
+///
+/// assert_eq!(new_json, json!({"key": [{"nestedKey": "newValue"}]}));
+/// ```
+pub fn write_nested_json_no_io(mut json : Value, keys: String, value: Value) -> Value {
+    // Makes the key variable to keep track of characters
+    let mut key = String::new();
+
+    // Iterates through every char while keeping track of the index
+    for (i , char) in keys.chars().enumerate() {
+        match char {
+            // If char is a '.', set json[key] equal to the next nested key
+            '.' => {
+                json[key] = write_nested_json_no_io(json[&key].clone(), keys.clone().split_at(i + 1).1.to_owned(), value);
+                break;
+            },
+            // If char is a '['
+            '[' => {
+                // Get the char from the string as a usize
+                let i_key = keys.get(i+1..i+2).unwrap().parse::<usize>().unwrap();
+                
+                // If the key doesn't exist, push the value and set the json equal to the new Vec
+                if json.as_array().unwrap().len() - 1 < i_key {
+                    let mut json_vec = json.as_array().unwrap().to_owned();
+
+                    json_vec.push(value);
+
+                    json = Value::Array(json_vec);
+                } else { // If the key exists, set it equal to the next nested value
+                    json[i_key] = write_nested_json_no_io(json[i_key].clone(), keys.clone().split_at(i + 3).1.to_owned(), value);
+                }
+                
+                // Escape the loop
+                break;
+            },
+            // If char is a ']' do nothing
+            ']' => (),
+            // If char is anything else, add it to the key
+            _ => key.push(char),
+        }
+
+        // If i is the last character, or if the next character is ']' and i is the second to last character
+        // Write the inputted value to the json
+        if i == keys.len() - 1 || (keys.get(i..i+1).unwrap() == "]".to_string() && i == keys.len() - 2){
+            json[key.clone()] = value.clone();
+        }
+  
+    }
+
+    // Returns the json object
+    json
+}
+
 pub fn write_config(json_key: String, value: &str) {
-    write_json(constants::get_config_json_path(), json_key, value.to_string());
+    write_json(
+        constants::get_config_json_path(),
+        json_key,
+        value.to_string(),
+    );
+}
+
+/// Iterate over a json object and return a Vec of key values
+///
+/// # Arguments
+/// * `json_key : &str` - Key to search for
+/// * `json : &Value` - JSON object
+///
+/// # Returns
+/// 'Vec<String>' Contains all the found values
+pub fn iterate_json(json_key: &str, json: &Value) -> Vec<String> {
+    let mut entries: Vec<String> = Vec::new();
+
+    if json.is_array() {
+        for value in json.as_array().unwrap().to_vec() {
+            for v in iterate_json_map(json_key, &value) {
+                entries.push(v.to_string());
+            }
+        }
+    } else {
+        for v in iterate_json_map(json_key, json) {
+            entries.push(v);
+        }
+    }
+
+    entries
+}
+
+fn iterate_json_map(json_key: &str, json: &Value) -> Vec<String> {
+    let mut entries: Vec<String> = Vec::new();
+
+    for value in json.as_object().unwrap() {
+        let (key, v) = value;
+        if key == json_key {
+            entries.push(v.to_string());
+        } else if v.is_object() {
+            for val in iterate_json(json_key, &v) {
+                entries.push(val.to_string());
+            }
+        }
+    }
+
+    entries
+}
+
+pub fn get_json_length(json: &Value) -> u32 {
+    let mut size: u32 = 0;
+
+    if json.is_array() {
+        for v in json.as_array().unwrap().to_vec() {
+            for _ in v.as_object().unwrap() {
+                size += 1;
+            }
+        }
+    } else {
+        for _ in json.as_object().unwrap() {
+            size += 1;
+        }
+    }
+
+    size
 }
 
 /// Resets the config
@@ -220,7 +369,10 @@ pub fn reset_config() {
 /// Downloads launch.json from the resource URL
 #[tauri::command]
 pub fn get_launch_json() {
-    let _ = download_files("https://madman-modding.github.io/FalconsEsportsOverlaysData/launch.json", "launch.json");
+    let _ = download_files(
+        "https://madman-modding.github.io/FalconsEsportsOverlaysData/launch.json",
+        "launch.json",
+    );
 }
 
 /// Get all the available overlays
@@ -234,7 +386,12 @@ pub fn get_name_map() -> Result<HashMap<String, Value>, String> {
         return Err("Error Reading JSON".to_string());
     }
 
-    Ok(object.unwrap().clone().into_iter().map(|(k, v)| (k, v.clone())).collect())
+    Ok(object
+        .unwrap()
+        .clone()
+        .into_iter()
+        .map(|(k, v)| (k, v.clone()))
+        .collect())
 }
 
 /// Get all the latest version for all the overlays
@@ -250,7 +407,12 @@ pub fn get_versions() -> Result<HashMap<String, Value>, String> {
         return Err("Error Reading JSON".to_string());
     }
 
-    Ok(object.unwrap().clone().into_iter().map(|(k, v)| (k, v.clone())).collect())
+    Ok(object
+        .unwrap()
+        .clone()
+        .into_iter()
+        .map(|(k, v)| (k, v.clone()))
+        .collect())
 }
 
 /// Get the list of all the overlays local versions
@@ -264,25 +426,35 @@ pub fn get_local_versions() -> Result<HashMap<String, Value>, String> {
         return Err("Error Reading JSON".to_string());
     }
 
-    Ok(object.unwrap().clone().into_iter().map(|(k, v)| (k, v.clone())).collect())
+    Ok(object
+        .unwrap()
+        .clone()
+        .into_iter()
+        .map(|(k, v)| (k, v.clone()))
+        .collect())
 }
 
 /// Gets the latest app version
 #[tauri::command]
 pub fn get_app_version() -> Result<HashMap<String, Value>, String> {
     let json = open_json(get_launch_json_path())["appVersion"].clone();
-    
+
     let object = json.as_object();
 
     if object.is_none() {
         return Err("Error Reading JSON".to_string());
     }
 
-    Ok(object.unwrap().clone().into_iter().map(|(k, v)| (k, v.clone())).collect())
+    Ok(object
+        .unwrap()
+        .clone()
+        .into_iter()
+        .map(|(k, v)| (k, v.clone()))
+        .collect())
 }
 
 /// Default settings for the config
-fn get_default_json_data() -> serde_json::Value {
+fn get_default_json_data() -> Value {
     json!({
         "appColor": "#bf0f35",
         "columnColor": "#ffffff",
--- src-tauri/src/handlers/obs_handler.rs
+++ src-tauri/src/handlers/obs_handler.rs
@@ -1,18 +1,32 @@
 
+use std::{fs, path::Path};
+
+use serde_json::{json, Value};
 use users::get_current_username;
 
-use super::json_handler;
+use crate::handlers::json_handler::{get_json_length, iterate_json, read_json_as_value, write_nested_json_no_io};
 
 pub fn inject(profile: &str, scene: &str) {
     let path = format!("{}{}.json", get_scene_path(), profile);
 
-    println!("{}", path);
+    println!("{}", &path);
 
-    let json = json_handler::read_json("sources", path);
+    let json = read_json_as_value(path.clone());
     
-    println!("{}", json);
-}
+    let items = json["sources"][0]["settings"]["items"].clone();
 
+    let entries = iterate_json("name", &items);
+
+    if !entries.contains(&"Falcons Esports Overlays Browser".to_string()) {
+        println!("Browser not found!\nSHAME!");
+
+        let item_count = get_json_length(&items);
+
+        let new_json = write_nested_json_no_io(json, format!("sources.[0]settings.items.[{}]", item_count + 1), get_obs_browser_source());
+
+        let _ = fs::write(Path::new("/home/mad/Desktop/test.json"), serde_json::to_string_pretty(&new_json).expect("Error Serializing JSON"));
+    }
+}
 
 pub fn get_profiles() -> Result<Vec<String>, String> {
     let path = get_profile_path();
@@ -68,4 +82,103 @@ fn get_scene_path() -> String {
         "linux" => format!("/home/{}/.var/app/com.obsproject.Studio/config/obs-studio/basic/scenes/", get_user()),
         _ => "".to_string()
     }
-} 
+}
+
+pub fn get_obs_browser_source() -> Value {
+    json!({
+        "name": "Falcons Esports Overlays Browser",
+        "source_uuid": "818eaee2-1c91-41be-91ff-9dc1a504a21a",
+        "visible": true,
+        "locked": false,
+        "rot": 0.0,
+        "scale_ref": {
+            "x": 1920.0,
+            "y": 1080.0
+        },
+        "align": 5,
+        "bounds_type": 0,
+        "bounds_align": 0,
+        "bounds_crop": false,
+        "crop_left": 0,
+        "crop_top": 0,
+        "crop_right": 0,
+        "crop_bottom": 0,
+        "id": 3,
+        "group_item_backup": false,
+        "pos": {
+            "x": 0.0,
+            "y": 0.0
+        },
+        "pos_rel": {
+            "x": -1.7777777910232544,
+            "y": -1.0
+        },
+        "scale": {
+            "x": 1.0,
+            "y": 1.0
+        },
+        "scale_rel": {
+            "x": 1.0,
+            "y": 1.0
+        },
+        "bounds": {
+            "x": 0.0,
+            "y": 0.0
+        },
+        "bounds_rel": {
+            "x": 0.0,
+            "y": 0.0
+        },
+        "scale_filter": "disable",
+        "blend_method": "default",
+        "blend_type": "normal",
+        "show_transition": {
+            "duration": 0
+        },
+        "hide_transition": {
+            "duration": 0
+        },
+        "private_settings": {}
+    }
+
+    )
+}
+
+pub fn get_obs_browser_config() -> Value {
+    json!({
+        "prev_ver": 520093699,
+        "name": "Falcons Esports Overlays Browser",
+        "uuid": "818eaee2-1c91-41be-91ff-9dc1a504a21a",
+        "id": "browser_source",
+        "versioned_id": "browser_source",
+        "settings": {
+            "width": 1920,
+            "height": 1080,
+            "url": "http://localhost:8080",
+            "fps": 60,
+            "css": ""
+        },
+        "mixers": 255,
+        "sync": 0,
+        "flags": 0,
+        "volume": 1.0,
+        "balance": 0.5,
+        "enabled": true,
+        "muted": false,
+        "push-to-mute": false,
+        "push-to-mute-delay": 0,
+        "push-to-talk": false,
+        "push-to-talk-delay": 0,
+        "hotkeys": {
+            "libobs.mute": [],
+            "libobs.unmute": [],
+            "libobs.push-to-mute": [],
+            "libobs.push-to-talk": [],
+            "ObsBrowser.Refresh": []
+        },
+        "deinterlace_mode": 0,
+        "deinterlace_field_order": 0,
+        "monitoring_type": 0,
+        "private_settings": {}
+    })
+}
